78ba4cc6250fe050dfd6731109e63ad3d5da3bb7
Dca <dca35311@gmail.com>
2014-02-01 03:07:35 +0800

    Add spaces

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var fs      = require('fs');
 
 var _addr   = '新北市中和區中山路３段';
 
-fs.readFile(__dirname+'/Zip32_10301.csv' ,
-            'utf-8',
-            function read( err , data ) {
-    if (err) throw err;
-    data = data.split('\r\n');
-    console.log('查詢：', _addr);
-    console.log( fuzzy.filter( _addr , data )
-                            .map( function(rt) { 
-                                    return rt.string; 
-                                  }));
-});
+fs.readFile(__dirname + '/Zip32_10301.csv',
+    'utf-8',
+    function read(err, data) {
+        if (err) throw err;
+        data = data.split('\r\n');
+        console.log('查詢：', _addr);
+        console.log(fuzzy.filter(_addr, data).map(function (rt) {
+            return rt.string;
+        }));
+    }
+);
